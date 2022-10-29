022d3089a279dc584a4749ebaf9e2adb0b7cc591
Miguel Angel Diaz D <migerodd888@gmail.com>
2022-10-29 11:18:48 -0500

    cambios xd

---
+++ script.jsx
@@ -0,0 +1,45 @@
+// ESTADO DE LOS AGENTES
+
+// id y nombres
+// "id" - "name"
+
+// total usuarios en el servicio 
+// "rMemberUsers" - "count"
+
+// total usuarios activos  - id 
+// "rActiveMembers" - "usersId" - "count"
+
+// De los usuarios conectados quienes estan interactuando
+//  rOnQueueUsers = "INTERACTING" - "usersId"
+
+// De los asesores que estan activos, quienes estan a la espera de una llamada.
+//  rOnQueueUsers = "IDLE" - "usersId"
+
+// De los asesores que estan activos cuales se encuentran en un estado ACW
+//  rOnQueueUsers = "ACW" - "usersId"
+
+
+// INDICADORES
+
+// id y nombres
+// "id" - "name"
+
+// numero de eventos - Cuantos de han producido desde el ultimo evento 
+// "aEvents" - 
+
+
+// Numero de llamadas de telefono 
+// "aCalls"
+
+// Tiempo de ACW
+// "aAcw"
+
+// Numero de ventas
+// "aSales"
+
+// Llamadas programadas
+// "aShedules"
+
+// Errores cometidos por el sistema de eventos
+// "aErrors"
+
